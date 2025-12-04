debcf541820157dce708bfca543cb6a82efe6575
maks-n <71183975+maks-n@users.noreply.github.com>
2025-12-03 20:59:13 -0500

    day-03 part-01

--- 2025/day-03/lobby.py
+++ 2025/day-03/lobby.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 if len(sys.argv) < 2:
-   print("Usage: python lobby.py <path>")
+   print("Usage: python lobby_star.py <path>")
    sys.exit(1)
 
 file_path = Path(sys.argv[1])
@@ -12,14 +12,15 @@ with open(file_path, 'r') as file:
     batteries = file.read().strip().split('\n')
     for battery in batteries:
         left = 0
-        for i in range(0, len(battery) - 1):
+        for i in range(len(battery) - 1):
             if battery[i] > battery[left]:
                 left = i
+
         right = left + 1
         for i in range(right, len(battery)):
             if battery[i] > battery[right]:
                 right = i
-        cur_joltage = int(battery[left]) * 10 + int(battery[right])
+        cur_joltage = int(battery[left] + battery[right])
         total_joltage += cur_joltage
 
 print(f"Total joltage: {total_joltage}")
